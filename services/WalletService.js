c930324b5b86c0e3868e868f756fe2b2ffce1981
Michael <enjoyweaver@gmail.com>
2023-09-28 13:57:15 -0500

    main

--- services/WalletService.js
+++ services/WalletService.js
@@ -1,15 +1,22 @@
 // WalletService.js
 import { DescriptorSecretKey, Mnemonic, Blockchain, Wallet, DatabaseConfig, Descriptor } from 'bdk-rn';
-import { Network, KeyChainKind } from 'bdk-rn/lib/lib/enums';
+import { Network, KeyChainKind, WordCount } from 'bdk-rn/lib/lib/enums';
 
 export const createWallet = async () => {
   try {
-    // Create a Descriptor
-    const mnemonic = await new Mnemonic().create(12); // Use the word count suitable for your case
+    // Create a Mnemonic
+    const mnemonic = await new Mnemonic().create(WordCount.WORDS12);
+
+    // Create a DescriptorSecretKey
     const descriptorSecretKey = await new DescriptorSecretKey().create(Network.Regtest, mnemonic);
+
+    // Get the public key from DescriptorSecretKey
     const descriptorPublicKey = await descriptorSecretKey.asPublic();
+
+    // Fingerprint is a placeholder; you may need to replace it
     const fingerprint = 'd1d04177';
 
+    // Create an external public descriptor
     const externalPublicDescriptor = await new Descriptor().newBip44Public(
       descriptorPublicKey,
       fingerprint,
@@ -17,15 +24,14 @@ export const createWallet = async () => {
       Network.Regtest
     );
 
-    // You might need to create a change descriptor in a similar manner if needed
-
     // Set up the Blockchain configuration for regtest
     const blockchainConfig = {
-      url: 'http://127.0.0.1:9050', // Update this with your Bitcoin Core regtest RPC address
+      url: 'http://127.0.0.1:18443', // Update this with your Bitcoin Core regtest RPC address
       rpcuser: 'admin',
       rpcpassword: 'password',
     };
 
+    // Create a Blockchain instance
     const blockchain = await new Blockchain().create(blockchainConfig);
 
     // Create a wallet instance
