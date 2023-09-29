9f8e47bf5270da920fe315620e37e9a7bc429ae3
inDemniFi Crypto <118511874+inDemniFi@users.noreply.github.com>
2023-09-29 17:53:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import WalletComponent from './components/WalletComponent';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
+import App from './App'; 
+import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => WalletComponent);
+AppRegistry.registerComponent(appName, () => App);
 
